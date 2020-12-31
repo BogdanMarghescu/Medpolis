c4dd8c4bd0ff509de5f749b39ff8014d1f087f85
Bogdan Marghescu <bogdan.marghescu99@gmail.com>
2020-12-31 04:55:41 +0200

    update medpolis

--- Medpolis/Medpolis/Main_menu_page.xaml.cs
+++ Medpolis/Medpolis/Main_menu_page.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Entity;
 
 namespace Medpolis
 {
@@ -20,9 +21,28 @@ namespace Medpolis
     /// </summary>
     public partial class Main_menu_page : Page
     {
+        private Clinica_MedpolisEntities medpolis_context = new Clinica_MedpolisEntities();
+        CollectionViewSource specComboBox;
+        CollectionViewSource pretViewSource;
+
         public Main_menu_page()
         {
             InitializeComponent();
+            specComboBox = ((CollectionViewSource)(FindResource("specialitateViewSource")));
+            pretViewSource = ((CollectionViewSource)(FindResource("specialitateServiciuViewSource")));
+            DataContext = this;
+        }
+
+        private void preturi_tab_menu_Loaded(object sender, RoutedEventArgs e)
+        {
+            serviciuDataGrid.SelectedIndex = -1;
+            medpolis_context.Specialitate.Load();
+            specComboBox.Source = medpolis_context.Specialitate.Local;
+        }
+
+        private void specialitateComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            serviciuDataGrid.SelectedIndex = -1;
         }
     }
 }
